a87d2ffabb0fed18be5a388fc7fd61f1dc79c9ec
wallenium <wallenium@eqdkp-plus.eu>
2014-12-14 13:42:26 +0000

    NO CHANGES - file headers updated

--- includes/epgp_parser.class.php
+++ includes/epgp_parser.class.php
@@ -1,19 +1,22 @@
 <?php
- /*
- * Project:     EQdkp-Plus Raidlogimport
- * License:     Creative Commons - Attribution-Noncommercial-Share Alike 3.0 Unported
- * Link:		http://creativecommons.org/licenses/by-nc-sa/3.0/
- * -----------------------------------------------------------------------
- * Began:       2009
- * Date:        $Date: 2012-11-11 16:50:58 +0100 (So, 11. Nov 2012) $
- * -----------------------------------------------------------------------
- * @author      $Author: wallenium $
- * @copyright   2008-2009 hoofy_leon
- * @link        http://eqdkp-plus.com
- * @package     raidlogimport
- * @version     $Rev: 12431 $
+/*	Project:	EQdkp-Plus
+ *	Package:	EPGPimport Plugin
+ *	Link:		http://eqdkp-plus.eu
  *
- * $Id: epgp_parser.class.php 12431 2012-11-11 15:50:58Z wallenium $
+ *	Copyright (C) 2006-2015 EQdkp-Plus Developer Team
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU Affero General Public License as published
+ *	by the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU Affero General Public License for more details.
+ *
+ *	You should have received a copy of the GNU Affero General Public License
+ *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 if(!defined('EQDKP_INC')) {
