85405f0efd90ce24f14b1ed3abefe29b44346285
GodMod <godmod23@web.de>
2019-11-11 17:36:40 +0100

    Added Support for CEPGP

--- includes/epgp_parser.class.php
+++ includes/epgp_parser.class.php
@@ -30,6 +30,10 @@ if(!class_exists('epgp_parser')) {
 		}
 		
 		public function parse($strLog, $intEventID, $intItempoolID){
+			if($this->config->get('dkp_easymode')){
+				$intItempoolID = $this->pdh->get('event', 'def_itempool', array($intEventID));
+			}
+			
 			if ($objLog = json_decode($strLog)){
 				$strTime = $this->time->date("Y-m-d H:i", intval($objLog->timestamp));
 				$intTime = intval($objLog->timestamp);
@@ -41,7 +45,6 @@ if(!class_exists('epgp_parser')) {
 					}
 				}
 				
-			
 				//Build itemList
 				$blnRaidItemList = false;
 				$arrItemMembernameServernameList = array();
@@ -159,7 +162,7 @@ if(!class_exists('epgp_parser')) {
 					$floatAdjustement = $floatEP - $floatCurrentEP;
 					if ($floatAdjustement != 0){
 						//create adjustment
-						$arrAdjustment[] = array(
+						$arrAdjustment[$floatAdjustement][] = array(
 							'value' => $floatAdjustement,
 							'member'=> $intMemberID,
 							'reason'=> 'EP, Snapshot '.$strTime,
@@ -169,7 +172,7 @@ if(!class_exists('epgp_parser')) {
 					$floatGPItem = $floatGP - $floatCurrentGP - ((isset($arrItemMemberList[$strFullMembername])) ? $arrItemMemberList[$strFullMembername] : 0);
 
 					//create dummy GP Item
-					$arrItem[] = array(
+					$arrItem[(float)$floatGPItem][] = array(
 						'value'		=> (float)$floatGPItem,
 						'name'		=> 'GP, Snapshot '.$strTime,
 						'gameid'	=> 0,
@@ -184,14 +187,23 @@ if(!class_exists('epgp_parser')) {
 
 				if ($raid_upd){
 					//Add Adjustments
-					foreach ($arrAdjustment as $adj){
-						if ($adj['value'] == 0) continue;
-						$adj_upd[] = $this->pdh->put('adjustment', 'add_adjustment', array($adj['value'], $adj['reason'], $adj['member'], $intEventID, $raid_upd, $intTime));
+					foreach ($arrAdjustment as $val => $a){
+						if($val == 0) continue;
+						$arrMembers = array();
+						foreach($a as $adj){
+							$arrMembers[] = $adj['member'];	
+						}
+						$adj_upd[] = $this->pdh->put('adjustment', 'add_adjustment', array($adj['value'], $adj['reason'], $arrMembers, $intEventID, $raid_upd, $intTime));
 					}
-					$itempoolid = 1;
-					foreach ($arrItem as $item){
-						if ($item['value'] == 0) continue;
-						$item_upd[] = $this->pdh->put('item', 'add_item', array($item['name'], $item['member'], $raid_upd, $item['gameid'], $item['value'], $intItempoolID, $intTime));
+					
+					foreach ($arrItem as $val => $a){
+						if($val == 0) continue;
+						$arrMembers = array();
+						foreach($a as $item){
+							$arrMembers[] = $item['member'];	
+						}
+						
+						$item_upd[] = $this->pdh->put('item', 'add_item', array($item['name'], $arrMembers, $raid_upd, $item['gameid'], $item['value'], $intItempoolID, $intTime));
 					}
 					//Add Items
 					foreach ($arrItemList as $item){
@@ -212,9 +224,139 @@ if(!class_exists('epgp_parser')) {
 					$this->pdh->process_hook_queue();
 					return $raid_upd;
 				}
+			} else {
+				//Is it a CVS for CEPGP?
+				/* Charname, Class, Rank, EP, GP, EP/GP
+				 * Sneakums,Warlock,Member,0,1,0
+					Spellcaster,Mage,Initiate,0,1,0
+					Sprunky,Rogue,Member,16,29,0.55
+				 */
+				if(strpos($strLog, ',') !== false){
+					$arrMember = array();
+					$arrAdjustment = array();
+					
+					$strTime = $this->time->date("Y-m-d H:i", $this->time->time);
+					
+					$arrLines = explode("\r\n", trim($strLog));
+					foreach($arrLines as $strLine){
+						$arrMemberItem = explode(',', trim($strLine));
+						
+						list($membername, $servername) = explode('-', $arrMemberItem[0]);
+						$servername = preg_replace_callback(
+								"/([^A-Z\'\"\-; ])([A-Z])/",
+								function($m) { return $m[1].' '.$m[2]; },
+								$servername
+								);
+						
+						$strMembername = trim($membername);
+						
+						$strServername = (isset($servername) && strlen($servername)) ? trim($servername) : $this->config->get('servername');
+						
+						$strFullMembername = $strMembername.'-'.unsanitize($strServername);
+						
+						$floatEP = (float)$arrMemberItem[3];
+						$floatGP = (float)$arrMemberItem[4];
+						$strRankname = $arrMemberItem[2];
+						$strClassname = $arrMemberItem[1];
+						$intClassID = $this->resolveClassname($strClassname);
+						
+						//Get MemberID, if none, create member
+						$intMemberID = $this->pdh->get('member', 'id', array(sanitize($strMembername), array('servername' => sanitize($strServername))));
+						
+						if (!$intMemberID){
+							//create new Member
+							$data = array(
+									'name' 		=> $strMembername,
+									'lvl' 		=> 0,
+									'raceid'	=> 0,
+									'classid'	=> $intClassID,
+									'rankid'	=> $this->pdh->get('rank', 'byname', array($strRankname)),
+									'servername'=> $strServername,
+							);
+							$intMemberID = $this->pdh->put('member', 'addorupdate_member', array(0, $data));
+							$this->pdh->process_hook_queue();
+							
+							$floatCurrentEP = 0;
+							$floatCurrentGP = 0;
+						} else {
+							$arrMDKPools = $this->pdh->get('event', 'multidkppools', array($intEventID));
+							$intMultidkpID = $arrMDKPools[0];
+							$floatCurrentEP = $this->pdh->get('epgp', 'ep', array($intMemberID, $intMultidkpID, false, false));
+							$floatCurrentGP = $this->pdh->get('epgp', 'gpwithbp', array($intMemberID, $intMultidkpID, false, false));
+						}
+						
+						$floatAdjustement = $floatEP - $floatCurrentEP;
+						if ($floatAdjustement != 0){
+							//create adjustment
+							$arrAdjustment[(float)$floatAdjustement][] = array(
+									'value' => (float)$floatAdjustement,
+									'member'=> $intMemberID,
+									'reason'=> 'EP, Snapshot '.$strTime,
+							);
+						}
+						
+						$floatGPItem = $floatGP - $floatCurrentGP;
+						
+						//create dummy GP Item
+						$arrItem[(float)$floatGPItem][] = array(
+								'value'		=> (float)$floatGPItem,
+								'name'		=> 'GP, Snapshot '.$strTime,
+								'gameid'	=> 0,
+								'member'	=> $intMemberID,
+						);
+						
+						$arrMember[] = $intMemberID;
+					}
+
+					//Create raid with value 0
+					$raid_upd = $this->pdh->put('raid', 'add_raid', array($this->time->time, $arrMember, $intEventID, 'EPGP-Snapshot '.$strTime, 0));
+					$adj_upd = $item_upd = array();
+					
+					if ($raid_upd){
+						//Add Adjustments
+						foreach ($arrAdjustment as $val => $a){
+							if($val == 0) continue;
+							$arrMembers = array();
+							foreach($a as $adj){
+								$arrMembers[] = $adj['member'];
+							}
+							$adj_upd[] = $this->pdh->put('adjustment', 'add_adjustment', array($adj['value'], $adj['reason'], $arrMembers, $intEventID, $raid_upd, $intTime));
+						}
+						
+						foreach ($arrItem as $val => $a){
+							if ($val == 0) continue;
+							$arrMembers = array();
+							foreach($a as $item){
+								$arrMembers[] = $item['member'];
+							}
+							$item_upd[] = $this->pdh->put('item', 'add_item', array($item['name'], $arrMembers, $raid_upd, $item['gameid'], $item['value'], $intItempoolID, $intTime));
+						}
+												
+						$this->pdh->process_hook_queue();
+						return $raid_upd;
+					}
+					
+					
+				}
+				
 			}
+			
 			return false;
 		}
+
+	
+		private function resolveClassname($strClassname){
+			if(strtolower($strClassname) == strtolower('DEATHKNIGHT')){
+				$strClassname = "Death Knight";
+			}
+			if(strtolower($strClassname) == strtolower('DEMONHUNTER')){
+				$strClassname = "Demon Hunter";
+			}
+			
+			$intClassID = $this->game->get_id('classes', $strClassname);
+			return $intClassID;
+		}
+		
 	}
 }
 ?>
